0d13cb6802f402d1f6ecc8934a7e790c144f1374
shua0326 <shuyanghua123@gmail.com>
2026-08-29 23:43:31 +1000

    adjusted auth logic

--- BE/src/controllers/auth/authController.ts
+++ BE/src/controllers/auth/authController.ts
@@ -9,35 +9,21 @@ export async function handleSocialAuthCallback(req: Request, res: Response): Pro
             return;
         }
 
-        console.log(req.supabaseUser);
-
-        let dbUser = req.dbUser;
-
-        if (!dbUser) {
-            try {
-                let user = req.supabaseUser;
-                const email = user.user_metadata.email ?? user.email;
-                if (!email) {
-                    res.status(400).json({ error: 'Email is required' });
-                    return;
-                }
-
-                dbUser = await prisma.mailUser.create({
-                    data: {
-                        id: user.id,
-                        email: email,
-                        username: email
-                    }
-                });
-
-                req.dbUser = dbUser;
-
-            } catch (dbError) {
-                console.error('Error updating user ID:', dbError);
-                res.status(500).json({ error: 'Internal server error' });
-                return;
-            }
+        const user = req.supabaseUser;
+        const email = user.user_metadata.email ?? user.email;
+        if (!email) {
+            res.status(400).json({ error: 'Email is required' });
+            return;
         }
+
+        const dbUser = await prisma.mailUser.upsert({
+            where: { id: user.id },
+            update: {},
+            create: { id: user.id, email: email, username: email }
+        });
+
+        req.dbUser = dbUser;
+        
         // Successfully authenticated and processed the user
         res.status(200).json({
             message: 'User authenticated successfully',
